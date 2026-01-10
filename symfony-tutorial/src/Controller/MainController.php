6873891a8c5e0bc681ed402b86e692bf29607786
Mirco Baalmans <mircobaalmans@live.nl>
2026-01-10 19:21:38 +0100

    Add MainController with index route returning a response

---
+++ symfony-tutorial/src/Controller/MainController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController{
+    
+    #[Route('/')]
+    public function index(){
+        return new Response("Response");
+    }
+}
