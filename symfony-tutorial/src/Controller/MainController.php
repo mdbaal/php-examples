f1cb3b8f5315ccadf49e8031166374641628f69f
Mirco Baalmans <mircobaalmans@live.nl>
2026-01-11 13:35:31 +0100

    Added Twig and basic view

--- symfony-tutorial/src/Controller/MainController.php
+++ symfony-tutorial/src/Controller/MainController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController{
+class MainController extends AbstractController{
     
     #[Route('/')]
     public function index(): Response
     {
-        return new Response("Response");
+        return $this->render('main/homepage.html.twig',["message" => "Helloo"]);
     }
 }
